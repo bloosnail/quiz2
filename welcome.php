88b127a6f9eab1bf1f8759e826efab2e0b17c106
Daniel Hui <dah124@pitt.edu>
2017-03-27 19:09:54 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,21 +5,22 @@
 	</head>
 	<body>
 
-	<h1>Quiz 2</h1>
-	<p>Sign up for free cookies!</p>
+		<h1>Quiz 2</h1>
+		<p>Sign up for free cookies!</p>
 	
-	<?php
-	if (isset($_COOKIE["fname"])){
-		echo "Hi, " . $_COOKIE["fname"] . ", you recently signed up with the email address: " .
-		$_COOKIE["email"] . ", thank you!"
-	?>
+		<?php
+			if (isset($_COOKIE["fname"])){
+				echo "Hi, " . $_COOKIE["fname"] . ", you recently signed up with the email address: " .
+				$_COOKIE["email"] . ", thank you!"
+			}
+		?>
 	
-	<form action="register.php" method="post">
-		First name: <input type="text" name="fname"><br>
-		Last name: <input type="text" name="lname"><br>
-		E-mail: <input type="text" name="email"><br>
-		<input type="submit">
-	</form>
+		<form action="register.php" method="post">
+			First name: <input type="text" name="fname"><br>
+			Last name: <input type="text" name="lname"><br>
+			E-mail: <input type="text" name="email"><br>
+			<input type="submit">
+		</form>
 
-</body>
+	</body>
 </html>
